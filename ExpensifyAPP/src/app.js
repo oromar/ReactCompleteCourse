61b514d54b098b981fc48870040a07d8c2efdf91
Oromar Dantas <oromar.melo@gmail.com>
2018-05-25 18:04:57 -0300

    Boilerplate

---
+++ ExpensifyAPP/src/app.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'normalize.css/normalize.css'
+import './styles/styles.scss'
+  
+const app = document.getElementById('app')
+  
+ReactDOM.render(<p>This is my boilerplate!</p>, app)
+  
