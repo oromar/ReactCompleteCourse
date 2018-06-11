cde0e144fcf98515cd7c26a4a0be9e5bfa31c06a
oromar <oromar.melo@gmail.com>
2018-06-10 23:49:06 -0300

    expenses reducer Unit tests

--- ExpensifyAPP/src/test/reducers/expense.test.js
+++ ExpensifyAPP/src/test/reducers/expense.test.js
@@ -1,5 +1,24 @@
 import expenseReducer from '../../reducers/expenses'
 
-test('should be', () => {
-    
+test('should set default values', () => {
+    expect(expenseReducer(undefined, {action: '@@INIT'}))
+    .toEqual([])
 })
+
+test('should set expense values', () => {
+    expect(expenseReducer(undefined, { type: 'ADD_EXPENSE', expense: {
+        id: '1',
+        amount: 100,
+        createdAt: 0,
+        note: '',
+        description: 'Rent'
+    }}))
+    .toEqual([{
+        id: '1',
+        amount: 100,
+        createdAt: 0,
+        note: '',
+        description: 'Rent'
+    }])
+})
+
